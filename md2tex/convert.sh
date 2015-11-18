ae66875dc4e3cccc900edb7863311dc451b8793e
Richard Dong <richarddzh@gmail.com>
2015-11-18 16:07:57 +0800

    revise bash script

--- md2tex/convert.sh
+++ md2tex/convert.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # convert all markdown files in the current dir
+pybin=python3
+command -v $pybin > /dev/null 2>&1 || pybin=python
 for f in *.md
 do
   fn=`basename $f .md`
-  python3 md2tex.py -o "$fn.tex" "$f"
+  $pybin md2tex.py -o "$fn.tex" "$f"
 done
 
